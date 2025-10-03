3123b65cd5ae43c80707e8776ec50c8ac736f839
sebastiangiampani-INGS <sebastian.giampani@colegioguidospano.com>
2025-10-03 08:30:30 -0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,1 +1,12 @@
+const config = require('../config');
+let impl;
 
+if (config.storage.backend === 'MYSQL') {
+  impl = require('./mysql');
+  console.log('[DB] Usando MySQL');
+} else {
+  impl = require('./sheets');
+  console.log('[DB] Usando Google Sheets');
+}
+
+module.exports = impl;
